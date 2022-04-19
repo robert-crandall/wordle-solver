df641ab09ce09f5aef073be052aee89bc5f25b17
robert-crandall <robert-crandall@github.com>
2022-04-19 13:05:42 -0700

    Allow baseline testing

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Wordle Solver
+
+Small Wordle solver. Primarily used to learn Ruby a bit better.
+
+## Solving a problem
+
+```shell
+ruby solver.rb
+```
+
+## Testing algorith efficiency
+
+```shell
+# Create a baseline
+ruby tester.rb --name baseline --small
+# Run this to create full baseline: ruby tester.rb --name baseline
+# Make change
+# Test the change 
+ruby tester.rb --name test --small
+```
--- tester.rb
+++ tester.rb
@@ -1,22 +1,51 @@
 require_relative './wordle.rb'
-
+require 'getoptlong'
 
 possible_answers = File.read('possible_answers.txt').split
-test_name = 'baseline'
+
+# https://ruby-doc.org/stdlib-3.1.1/libdoc/getoptlong/rdoc/GetoptLong.html
+opts = GetoptLong.new(
+  [ '--name', '-n',  GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--small', '-s', GetoptLong::NO_ARGUMENT ]
+)
+
+test_name = nil
+@small = false
+
+opts.each do |opt, arg|
+  case opt
+  when '--name'
+    test_name = arg
+  when '--small'
+    @small = true
+  end
+end
+
 
 filename = "./cache/#{test_name}.json"
 small_filename = "./cache/#{test_name}_small.json"
 
-if(File.exist?(filename))
+baseline_filename = './cache/baseline.json'
+baselinesmall_filename = './cache/baseline_small.json'
+
+unless File.exist?(baseline_filename)
+  unless test_name == 'baseline'
+    puts 'Baseline file needed before making improvements!'
+    exit!
+  end
+end
+
+if File.exist?(filename) && !@small
   puts 'Full results already exists for this test run! Delete it or rename the test'
-  puts "Hint: `rm #{filename}`"
+  puts "Hint: `rm -f #{filename}`"
   exit!
 end
 
-unless File.exist?(small_filename)
+if !File.exist?(small_filename) || @small
   puts 'No small test was run. Running that now.'
   possible_answers = File.read('possible_answers_small.txt').split
   filename = small_filename
+  baseline_filename = baselinesmall_filename
 end
 
 
@@ -41,11 +70,21 @@ def count_answer_tries(answer)
   @word_results[answer] = i
 end
 
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+total_words = possible_answers.length
+i = 0
 
 possible_answers.each do |answer|
+  if i % 100 == 0
+    puts "On word #{i} of #{total_words}"
+  end
   self.count_answer_tries(answer)
+  i += 1
 end
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
+elapsed = ending - starting
 
 hash_average = @word_results.values.sum(0.0) / @word_results.length
 puts "Hash average: #{hash_average}"
@@ -53,13 +92,27 @@ puts "Hash average: #{hash_average}"
 failed = @word_results.select { |_word, tries| tries > 6 }
 
 results = {
-  test_name => {
     'average' => hash_average,
+    'execution_seconds' => elapsed.round,
     'failed_count' => failed.length,
-    'failed' => failed,
-    'words' => @word_results
-  }
+    'failed_words' => failed,
+    'all_words' => @word_results
 }
+
+if File.exist?(baseline_filename) && test_name != 'baseline'
+  file = File.read(baseline_filename)
+  baseline_results = JSON.parse(file)
+
+  baseline_average = baseline_results['average']
+
+  puts "Baseline vs #{test_name}"
+  puts "Avg Count: #{baseline_results['average']} vs #{hash_average.round(2)} #{hash_average < baseline_results['average'] ? ' - Improvement!' : ''}"
+  puts "Time: #{baseline_results['execution_seconds']} vs #{elapsed.round} #{elapsed.round < baseline_results['execution_seconds'] ? ' - Improvement!' : ''}"
+  puts "Failed Count: #{baseline_results['failed_count']} vs #{failed.length} #{failed.length < baseline_results['failed_count'] ? ' - Improvement!' : ''}"
+
+end
+
+
 
 File.open(filename,'w') do |f|
   f.write(JSON.pretty_generate(results))
